258dac16e51b608d49690b22f20dd24cf43cfecc
Allen Simpson <allen.simpson@motionencoding.com>
2018-04-05 02:44:58 -0600

    Added tests for Prim, PrimJarnik, and Kruskal Algorithms

--- src/Driver.java
+++ src/Driver.java
@@ -3,17 +3,45 @@ import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.List;
 
-import sun.tools.jar.resources.jar;
-
 public class Driver {
     public static void main (String args[]) {
         String pathName = args.length>0 ? args[0] :  "./input.in";
         AdjacencyMatrix am = new AdjacencyMatrix(readFile(pathName));
+        testPrim(am);
+        testPrimJarnik(am);
+        testKruskal(am);
         testWarshall(am);
     }
+    private static void testPrimJarnik(AdjacencyMatrix am) {
+        System.out.println("Running PrimJarnik:");
+        System.out.println("");
+        PrimJarnik primJ = new PrimJarnik();
+        PrimJarnik.Edge[] edges=primJ.primJarnik(am,am.vertices[0]);
+        System.out.println(edges[0].arrayToString(edges));
+        System.out.println("");
+        
+    }
     private static void testWarshall(AdjacencyMatrix am) {
+        System.out.println("Running Warshall:");
+        System.out.println("");
         Warshall warshall = new Warshall();
         warshall.floydWarshall(am.getGraph(),am);
+        System.out.println("");
+    }
+    private static void testPrim(AdjacencyMatrix am) {
+        System.out.println("Running Prim:");
+        System.out.println("");
+        Prim prim = new Prim();
+        prim.PrimJarnik(am.getGraph());
+        System.out.println("");
+
+    }
+    private static void testKruskal(AdjacencyMatrix am) {
+        System.out.println("Running Kruskal:");
+        System.out.println("");
+        Kruskal kurskal = new Kruskal();
+        kurskal.JosephKruskal(am.getGraph());
+        System.out.println("");
     }
     private static String readFile (String pathName) {
         try {
