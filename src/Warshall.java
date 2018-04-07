3e8167199265c13f83f8435f9f460f1d5ccaa7e8
William <William@BEASTMACHINE>
2018-04-07 17:09:57 -0600

    Removed main from Warshall.

--- src/Warshall.java
+++ src/Warshall.java
@@ -6,30 +6,31 @@ Author: William Kingsley
 import java.lang.*;
 public class Warshall {
     private void printGraph(int graph[][], AdjacencyMatrix am) {
-        for (String v : am.vertices){
-            System.out.print(" "+v+"  ");
+        for (String v : am.vertices) {
+            System.out.print(" " + v + "  ");
         }
         System.out.print("\n");
-        for (int i = 0; i < graph.length; i++) {    
-            for (int j = 0; j  < graph.length; j++) {
+        for (int i = 0; i < graph.length; i++) {
+            for (int j = 0; j < graph.length; j++) {
                 if (graph[i][j] == AdjacencyMatrix.INF) {
                     System.out.print("INF ");
-                }
-                else {
+                } else {
                     System.out.print(String.format("%3d", graph[i][j]) + " ");
                 }
             }
             System.out.println();
         }
     }
-    //Sum distance and account for wrap-around signed integers (INF). 
-    private static int addDistance (int d1, int d2) {
-        int s = d1+d2;
-        if (s<d1||s<d2) {
+
+    //Sum distance and account for wrap-around signed integers (INF).
+    private static int addDistance(int d1, int d2) {
+        int s = d1 + d2;
+        if (s < d1 || s < d2) {
             return AdjacencyMatrix.INF;
         }
         return s;
     }
+
     public void floydWarshall(int graph[][], AdjacencyMatrix am) {
         int dist[][] = new int[graph.length][graph.length];
         int i, j, k, x, y;
@@ -41,7 +42,7 @@ public class Warshall {
         }
         //Populating dist[][] with shortest paths
         for (k = 0; k < graph.length; k++) {
-            printGraph(dist,am);
+            printGraph(dist, am);
             System.out.println();
             for (x = 0; x < graph.length; x++) {
                 for (y = 0; y < graph.length; y++) {
@@ -52,16 +53,5 @@ public class Warshall {
                 }
             }
         }
-        //printGraph(dist);
-    }
-    public static void main(String[] args) {
-        int INF = AdjacencyMatrix.INF;
-        int graph[][] = { {0, 5, INF, 10},
-                          {INF, 0, 3, INF},
-                          {INF, INF, 0, 1},
-                          {INF, INF, INF, 0}
-                        };
-        Warshall warshall = new Warshall();
-        warshall.floydWarshall(graph);
     }
 }
